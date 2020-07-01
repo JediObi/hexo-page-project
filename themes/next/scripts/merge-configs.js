20f62388e95953999cf75d8d77d356c7d318fcc3
nomq <fc_prc@126.com>
2020-07-02 06:58:13 +0800

    issue

--- themes/next/scripts/merge-configs.js
+++ themes/next/scripts/merge-configs.js
@@ -53,36 +53,36 @@ hexo.on('generateAfter', function() {
   const https = require('https');
   const path = require('path');
   const env = require(path.normalize('../package.json'));
-  https.get('https://api.github.com/repos/theme-next/hexo-theme-next/releases/latest', {
-    headers: {
-      'User-Agent': 'Theme NexT Client'
-    }
-  }, res => {
-    var result = '';
-    res.on('data', data => {
-      result += data;
-    });
-    res.on('end', () => {
-      try {
-        var latest = JSON.parse(result).tag_name.replace('v', '').split('.');
-        var current = env.version.split('.');
-        var isOutdated = false;
-        for (var i = 0; i < Math.max(latest.length, current.length); i++) {
-          if (!current[i] || latest[i] > current[i]) {
-            isOutdated = true;
-            break;
-          }
-        }
-        if (isOutdated) {
-          hexo.log.warn(`Your theme NexT is outdated. Current version: v${current.join('.')}, latest version: v${latest.join('.')}`);
-          hexo.log.warn('Visit https://github.com/theme-next/hexo-theme-next/releases for more information.');
-        } else {
-          hexo.log.info('Congratulations! Your are using the latest version of theme NexT.');
-        }
-      } catch (e) {
-        hexo.log.error('Failed to detect version info. Error message:');
-        hexo.log.error(e);
-      }
-    });
-  });
+  // https.get('https://api.github.com/repos/theme-next/hexo-theme-next/releases/latest', {
+  //   headers: {
+  //     'User-Agent': 'Theme NexT Client'
+  //   }
+  // }, res => {
+  //   var result = '';
+  //   res.on('data', data => {
+  //     result += data;
+  //   });
+  //   res.on('end', () => {
+  //     try {
+  //       var latest = JSON.parse(result).tag_name.replace('v', '').split('.');
+  //       var current = env.version.split('.');
+  //       var isOutdated = false;
+  //       for (var i = 0; i < Math.max(latest.length, current.length); i++) {
+  //         if (!current[i] || latest[i] > current[i]) {
+  //           isOutdated = true;
+  //           break;
+  //         }
+  //       }
+  //       if (isOutdated) {
+  //         hexo.log.warn(`Your theme NexT is outdated. Current version: v${current.join('.')}, latest version: v${latest.join('.')}`);
+  //         hexo.log.warn('Visit https://github.com/theme-next/hexo-theme-next/releases for more information.');
+  //       } else {
+  //         hexo.log.info('Congratulations! Your are using the latest version of theme NexT.');
+  //       }
+  //     } catch (e) {
+  //       hexo.log.error('Failed to detect version info. Error message:');
+  //       hexo.log.error(e);
+  //     }
+  //   });
+  // });
 });
